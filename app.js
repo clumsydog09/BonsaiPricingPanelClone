473db1b035396548a2b2e7009688dbcf678276c2
Aditya Verma <aditya13.mitmpl2022@learner.manipal.edu>
2023-07-19 21:58:58 +0530

    made some changes to the navbar

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let div = document.querySelectorAll('div');
-let deleteLater = document.querySelector('#deleteLater');
-deleteLater.addEventListener('mousedown', () => {
+let body = document.querySelector('body');
+let deleteLater1 = document.querySelector('#deleteLater1');
+let deleteLater2 = document.querySelector('#deleteLater2');
+deleteLater1.addEventListener('mousedown', () => {
     for (let i = 0; i < div.length; i++) {
-        div[i].classList.toggle('divBorder');
+        div[i].classList.toggle('blackBorder');
     }
+}
+)
+deleteLater2.addEventListener('mousedown', () => {
+        body.classList.toggle('blackBorder');
 }
 )
